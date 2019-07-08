7fa4e1f5757472367785b5d14b3854a4279b1cd6
Kenneth Foo <pancakegiraffe@gmail.com>
2019-07-08 15:08:05 +1200

    First commit!

---
+++ Project_39/Assets/AutoSwitchScenes.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class AutoSwitchScenes : MonoBehaviour
+{
+	public string LevelToLoad;
+	private float timer = 20f;
+	private Text timerSeconds;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+		timerSeconds.text = "Time Left: " + timer.ToString("f0");
+		if (timer <= 0)
+		{
+			SceneManager.LoadScene(LevelToLoad);
+			
+			// Resets the timer.
+			timer = 20f;
+		}
+    }
+}
---
+++ Project_39/Assets/FPSCounter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+using UnityEngine.SceneManagement;
+
+public class FPSCounter : MonoBehaviour {
+
+	public double FPS { get; private set; }
+	
+	void Update () {
+		Scene scene = SceneManager.GetActiveScene();
+		
+		FPS = (double)(1f / Time.unscaledDeltaTime);
+		FPS = Math.Round((double)FPS, 2);
+		
+		string path = Application.persistentDataPath + scene.name + ".txt";
+
+        //Write some text to the test.txt file
+        StreamWriter writer = new StreamWriter(path, true);
+        writer.WriteLine(FPS);
+        writer.Close();
+		
+		Console.WriteLine(Application.persistentDataPath);
+	}
+}
